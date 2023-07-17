3ba8b912ba418065d5036188069f67655d66d7ff
dbstjdgur0@gmail.com <dbstjdgur0@gmail.com>
2023-07-17 19:50:42 +0900

    230717

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
 const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState("2020");
   const filterChageHandler = selectedYear => setFilteredYear(selectedYear);
-  const filterExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
@@ -18,7 +19,8 @@ const Expenses = props => {
         selected={filteredYear}
         onChangeFilter={filterChageHandler}
       />
-      <ExpensesList items={filterExpenses} />
+      <ExpensesChart expenses={filteredExpenses }/>
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
